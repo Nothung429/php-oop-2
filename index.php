bc7268cbb13c829df2b22b0e53528c5b28fcecaa
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-28 18:28:35 +0200

    fix

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<!-- Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche.
-L'e-commerce vende prodotti per gli animali.
-I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
-L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
-Il pagamento avviene con la carta di credito, che non deve essere scaduta  -->
 <?php
     require_once __DIR__ . "/constructors/ProductConstructor.php";
     require_once __DIR__ . "/constructors/UserConstructor.php";
@@ -67,3 +62,9 @@ Il pagamento avviene con la carta di credito, che non deve essere scaduta  -->
         </ul>
     </body>
 </html>
+
+<!-- Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche.
+L'e-commerce vende prodotti per gli animali.
+I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+L'utente potrà sia comprare i prodotti senza registrarsi, oppure iscriversi e ricevere il 20% di sconto su tutti i prodotti.
+Il pagamento avviene con la carta di credito, che non deve essere scaduta  -->
